f488739e1b57a957e58705beadfe0e6c4833d51a
Werner <werner.mueller2@e-mail.de>
2026-01-23 17:52:23 +0100

    chg: no serial support
    dark default screen
    Emergency uses assumeMotorOn derived from battery voltage
    screen selection per index
    webservice disabled, starts when switching to corresponding screen
    removed gefahrene_km

--- Firmware-3.0/lib/Controller/DefaultScreen.cpp
+++ Firmware-3.0/lib/Controller/DefaultScreen.cpp
@@ -17,7 +17,7 @@ void DefaultScreen::setup() {
     Serial.println("DefaultScreen::setup()");
 
     spr.setTextWrap(false);            // By default, long lines of text are set to automatically “wrap” back to the leftmost column.
-    spr.setTextColor(TFT_BLACK);
+    spr.setTextColor(fgColor, bgColor);
     spr.setTextSize (1);
     spr.setTextFont(6);
     initSinTab();
@@ -49,7 +49,7 @@ void DefaultScreen::updateData() {
         }
     }
     {
-        bool value = !gpsController.emergency();
+        bool value = gpsController.validData();
         if (value != showSattelite)
         {
             showSattelite = value;
@@ -135,7 +135,7 @@ void DefaultScreen::loop(ScreenArgs &args) {
         return;
     updateRequired = false;
 
-    spr.fillRect(0,0,TFT_HEIGHT, TFT_WIDTH, TFT_WHITE);
+    spr.fillRect(0,0,TFT_HEIGHT, TFT_WIDTH, bgColor);
     displaySpeed();
 
     displayDirection();
@@ -144,7 +144,7 @@ void DefaultScreen::loop(ScreenArgs &args) {
     displayTank();
     displayNoSattelite();
     displayOiling();
-    if (showRaining) spr.drawBitmap(30, 6, iconRaining(), 16, 16, TFT_BLACK);
+    if (showRaining) spr.drawBitmap(30, 6, iconRaining(), 16, 16, fgColor);
     spr.pushSprite(0,0);
 }
 
@@ -156,30 +156,30 @@ void DefaultScreen::displaySpeed()
     dtostrf(speed, 3, 0, tmp);
     spr.setTextSize (1);
     spr.setTextFont(6);
-    spr.setTextColor(TFT_RED);
+    spr.setTextColor(speedFgColor);
     int16_t w = spr.textWidth(tmp);
     spr.drawString(tmp, 158-w, 2);
-    spr.drawRoundRect(73, 1, 160-74, 40, 4, TFT_BLACK);  // Rahmen für die Geschwindigkeit
-    spr.setTextColor(TFT_BLACK);
+    spr.drawRoundRect(73, 1, 160-74, 40, 4, fgColor);  // Rahmen für die Geschwindigkeit
+    spr.setTextColor(fgColor);
 }
 
 void DefaultScreen::displayTank()
 {
     if (warningTankDisplay) {
-        spr.fillRect(1, 100, 72, 26, TFT_BLUE);    // Draws full bar in RED
+        spr.fillRect(1, 100, 72, 26, tankWarningColor);    // Draws full bar in RED
         char tmp[10];
         dtostrf(tankPercent, 2, 0, tmp);
         int l = strlen(tmp);
         tmp[l] = '%';
         tmp[l+1] = 0;
         spr.setTextFont(4);
-        spr.setTextColor(TFT_WHITE);
+        spr.setTextColor(tankWarningFgColor);
         uint16_t cwidth = spr.textWidth(tmp);
         spr.drawString(tmp, 36-cwidth/2, 102);
     } 
     byte v = map(tankPercent, 0, 100, 0, 72); // map percent to rect length
-    spr.fillRect(1, 100, v, 26, TFT_RED);      // Draws the bar depending on the sensor value
-    spr.drawRect(1, 100, 72, 26, TFT_BLACK);        // Border of the bar chart
+    spr.fillRect(1, 100, v, 26, tankBarColor);      // Draws the bar depending on the sensor value
+    spr.drawRect(1, 100, 72, 26, fgColor);        // Border of the bar chart
 }
 
 void DefaultScreen::displayTime()
@@ -207,7 +207,7 @@ void DefaultScreen::displayTime()
     spr.setTextSize (1);
     spr.setTextFont(4);
     spr.println(tmp);
-    spr.drawRoundRect(1, 45, 72, 23, 4, TFT_BLACK);  // Rahmen für die Zeit
+    spr.drawRoundRect(1, 45, 72, 23, 4, fgColor);  // Rahmen für die Zeit
 }
 
 void DefaultScreen::displayAlt()
@@ -220,7 +220,7 @@ void DefaultScreen::displayAlt()
     spr.setTextFont(4);
     int16_t w = spr.textWidth(tmp);
     spr.drawString(tmp, 70 - w,73);
-    spr.drawRoundRect(1, 72, 72, 24, 4, TFT_BLACK);
+    spr.drawRoundRect(1, 72, 72, 24, 4, fgColor);
 }
 
 void DefaultScreen::displayNoSattelite()
@@ -228,10 +228,10 @@ void DefaultScreen::displayNoSattelite()
     for (int i = 0; i < 6;i++) {
         int h=4 + i*7;
         if (i < (noSattelite - 2)) {
-            spr.fillRect (42 + i*5, 41 - h, 4, h, TFT_BLACK);
+            spr.fillRect (42 + i*5, 41 - h, 4, h, fgColor);
         } else {
-            spr.fillRect (42 + i*5, 41 - h, 4, h, TFT_WHITE);
-            spr.drawRect (42 + i*5, 41 - h, 4, h, TFT_BLACK);
+            spr.fillRect (42 + i*5, 41 - h, 4, h, bgColor);
+            spr.drawRect (42 + i*5, 41 - h, 4, h, fgColor);
         }
     }
 }
@@ -240,11 +240,11 @@ void DefaultScreen::displayOiling()
 {
     int h = 39;
     if (showOiling) {
-        spr.drawBitmap(3, 6, iconOilcan(), 16, 16, TFT_BLACK);
+        spr.drawBitmap(3, 6, iconOilcan(), 16, 16, fgColor);
     } else  {
-        spr.drawRect(1, 1, 20, h, TFT_BLACK);
+        spr.drawRect(1, 1, 20, h, fgColor);
         int p = h*oilingDistanceInPercent/100;
-        spr.fillRect(1, 1+p, 20, h-p, TFT_BLACK);
+        spr.fillRect(1, 1+p, 20, h-p, fgColor);
     }
 }
 
@@ -284,7 +284,7 @@ void DefaultScreen::drawScale(int cx, int cy, int r) {
         int x2 = cx + cosr * l;
         int y2 = cy + sinr * l;
 
-        spr.drawLine(x1, y1, x2, y2, TFT_BLACK);
+        spr.drawLine(x1, y1, x2, y2, fgColor);
     }
 }
 
@@ -294,8 +294,8 @@ void DefaultScreen::displayCompass() {
     static Complex w = Complex(-30,0)*scale;;
     static Complex s = Complex(0,-100)*scale;;
 
-    spr.fillCircle(center.real(), center.imag(), radius + 2, TFT_DARKGREY);
-    spr.drawCircle(center.real(), center.imag(), radius + 2, TFT_BLACK);
+    spr.fillCircle(center.real(), center.imag(), radius + 2, bgCompass);
+    spr.drawCircle(center.real(), center.imag(), radius + 2, fgColor);
     drawScale(center.real(), center.imag(), radius);
     if (showSattelite) {
         float rad = -PI*2*(direction+180)/360; 
@@ -307,12 +307,12 @@ void DefaultScreen::displayCompass() {
         Complex _s = center + s*rot;
         spr.fillTriangle(_n.real(), _n.imag(), _w.real(), _w.imag(), _e.real(), _e.imag(), TFT_BLUE);
         spr.fillTriangle(_s.real(), _s.imag(), _w.real(), _w.imag(), _e.real(), _e.imag(), TFT_GREEN);
-        spr.drawTriangle(_n.real(), _n.imag(), _w.real(), _w.imag(), _e.real(), _e.imag(), TFT_BLACK);
-        spr.drawTriangle(_s.real(), _s.imag(), _w.real(), _w.imag(), _e.real(), _e.imag(), TFT_BLACK);
+        spr.drawTriangle(_n.real(), _n.imag(), _w.real(), _w.imag(), _e.real(), _e.imag(), fgColor);
+        spr.drawTriangle(_s.real(), _s.imag(), _w.real(), _w.imag(), _e.real(), _e.imag(), fgColor);
     }
-    spr.fillCircle(center.real(), center.imag(), 12, TFT_DARKGREY);
-    spr.drawCircle(center.real(), center.imag(), 12, TFT_BLACK);
-    spr.fillCircle(center.real(), center.imag(), 4, TFT_BLACK);
+    spr.fillCircle(center.real(), center.imag(), 12, bgCompass);
+    spr.drawCircle(center.real(), center.imag(), 12, fgColor);
+    spr.fillCircle(center.real(), center.imag(), 4, fgColor);
 
 }
 
